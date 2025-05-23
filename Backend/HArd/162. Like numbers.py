6d40b5d9f8544f4d62c758c9f5b5fe960a2dd400
Dasha06 <dasha.nikitina20@yandex.ru>
2025-05-23 15:12:20 +0300

    task 162 is completed but not all tests is going through, don't know why

--- Backend/HArd/162. Like numbers.py
+++ Backend/HArd/162. Like numbers.py
@@ -1,26 +1,45 @@
-import sys
-from random import sample
+from itertools import combinations
 
 
 def main():
     love_nums = [5, 6, 10]
-    n = list(input())
+    n = input().strip()
     num_of_operations = int(input())
-    res = recursion(num_of_operations, n, love_nums)
-    print(res/num_of_operations)
 
+    possible_combinations = list(combinations(range(len(n)), 2))
+    current_set = set([n])
 
-def recursion(num_oper, n, love_nums):
-    if num_oper == 1:
-        count = 0
-        index1, index2 = sample(range(len(n)), 2)
-        n[index1], n[index2] = n[index2], n[index1]
-        for i in love_nums:
-            if int(''.join(n)) % i == 0:
+    for _ in range(num_of_operations):
+        next_set = set()
+        for num in current_set:
+            for pos1, pos2 in possible_combinations:
+                if pos1 == pos2:
+                    continue
+                if num[pos1] == num[pos2]:
+                    continue
+                new_num = changenum(num, pos1, pos2)
+                next_set.add(new_num)
+        if not next_set or next_set == current_set:
+            break
+        current_set = next_set
+
+    count = 0
+    for num in current_set:
+        int_num = int(num)
+        for loveNum in love_nums:
+            if int_num % loveNum == 0:
                 count += 1
-        return count
-    else:
-        recursion(num_oper - 1, n, love_nums)
+                break
+
+    print(count / len(current_set))
+
+
+def changenum(n, pos1, pos2):
+    if pos1 == pos2:
+        return n
+    n = list(n)
+    n[pos1], n[pos2] = n[pos2], n[pos1]
+    return ''.join(n)
 
 
 if __name__ == '__main__':
